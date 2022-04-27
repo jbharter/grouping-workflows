d3e3ab4cba8ac2d66a1c0cdf7b0537aedbde186d
Jacob Harter <jacob.harter@liveramp.com>
2022-04-27 12:39:27 -0500

    workflows

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+function onboard(user) {
+    core.error("Onboard has not been implemented yet.")
+    core.setFailed("Onboard has not been implemented yet.")
+}
+
+function offboard(user) {
+    core.info(`offboarding: ${user}`)
+}
+
 async function run() {
     try {
-//        core.info(`context: ${JSON.stringify(github.context)}`)
 
         const payload = github.context.payload
-        const client_payload = github.context.payload.client_payload
-        const action = github.context.payload.action
-
+        const client_payload = payload.client_payload
+        const action = payload.action
+        const users = client_payload.users
 
         //const action = core.getInput('action')
         //const targetUsers = core.getInput('targetUsers')
 
         //core.info(`payload: ${JSON.stringify(payload)}`)
-        core.info(`client_payload: ${JSON.stringify(client_payload)}`)
-        core.info(`action: ${JSON.stringify(payload.action)}`)
-        core.info(`event_name: ${github.context.eventName}`)
+        //core.info(`client_payload: ${JSON.stringify(client_payload)}`)
+        //core.info(`action: ${JSON.stringify(payload.action)}`)
         //core.info(`client_payload: ${core.getInput('client_payload')}`)
+        core.info(`Setting up to ${action}`)
+        switch (action) {
+            case "onboard":
+                users.forEach(onboard)
+                break
+            case "offboard":
+                users.forEach(offboard)
+                break
+            default:
+                core.error(`Unsupported action: ${action}`)
+                core.setFailed(`Unsupported action: ${action}`)
+        }
 
 
     } catch (error) {
