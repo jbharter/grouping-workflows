e392063a13a57d2982a1f7c067aaa781e8a5cc6d
Jacob Harter <jacob.harter@liveramp.com>
2022-04-27 12:05:36 -0500

    workflows

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import * as github from '@actions/github'
 
 async function run() {
     try {
-        core.info(`context: ${github.context}`)
+        core.info(`context: ${JSON.stringify(github.context)}`)
 
         const action = core.getInput('action')
         const targetUsers = core.getInput('targetUsers')
 
         core.info(`Obtained action: ${action}`)
         core.info(`Obtained targets: ${targetUsers}`)
+        core.info(`event_type: ${core.getInput('event_type')}`)
+        core.info(`client_payload: ${core.getInput('client_payload')}`)
+
 
     } catch (error) {
         core.setFailed(error.message)
